61cfc865d228371b75b53a55c327617b5b8c9240
thanaschai <thanaschai@gmail.com>
2023-01-17 16:27:08 +0700

    done

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+    int i, j;
+    T key ;
+    for (i = 1; i < N; i++)
+    {	
+        key = d[i];
+        j = i - 1;
+ 
+        while (j >= 0 && d[j] < key)
+        {
+            d[j + 1] = d[j];
+            j = j - 1;
+        }
+        d[j + 1] = key;
+
+		cout << "Pass " << i << ":" ;
+		for(int i = 0 ; i < 10 ;i++ ) cout << d[i] << " "   ; 
+		cout << endl ;
+    }
 
 }
 
@@ -17,3 +35,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
