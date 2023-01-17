8aa374b4e80baa5a002dcbb0850c91bc7dc3a653
thanaschai <thanaschai@gmail.com>
2023-01-17 17:06:27 +0700

    done

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 const int N = 5;
 
 void inputMatrix(double [][N]);
+	
 
 void findLocalMax(const double [][N], bool [][N]);
 
@@ -18,4 +19,35 @@ int main(){
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+void inputMatrix(double A[][N]){
+    for(int i = 0 ;i < N;i++){
+        cout << "Row " << i + 1<< ": " ; 
+        for(int j = 0 ; j < N ; j++){
+            cin >> A[i][j] ;    
+        }
+    }
+}
+void showMatrix(const bool A[][N]){
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < N ; j++){
+            cout << A[i][j] << " " ;
+    }
+    cout << endl ;
+    }
+}
+void findLocalMax(const double A[][N],bool B[][N]){
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < N ; j++ ){
+            B[i][j] = false ;
+        }
+    }
+    for(int i = 1 ; i < N - 1 ; i++){
+        for(int j = 1 ; j < N -1 ; j++ ){
+            if(A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j] && A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1]) 
+            {
+            B[i][j] = true ;
+            }
+        }
+    }
 }
